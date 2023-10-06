7f00cac81c9e8b82cf5b90b9d30f5c3e5fae5e62
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2023-10-06 14:07:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Player.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "WorldTransform.h"
+#include "Model.h"
+
+class Player {
+public:
+
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	void Update();
+
+	void Draw(ViewProjection& viewProjection);
+
+private:
+
+	WorldTransform worldTransform_;
+	 
+	Model* model_ = nullptr;
+
+	uint32_t textureHandle_ = 0u;
+
+};
